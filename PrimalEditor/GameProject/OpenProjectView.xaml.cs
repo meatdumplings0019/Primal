cb8b04a1cc943488980b307bc5629b09ccdf011f
YourMD <meatdumplings@163.com>
2025-04-03 22:36:23 +0800

    Base View

---
+++ PrimalEditor/GameProject/OpenProjectView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace PrimalEditor.GameProject;
+
+public partial class OpenProjectView : UserControl
+{
+    public OpenProjectView()
+    {
+        InitializeComponent();
+    }
+}
